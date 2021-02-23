e38695a4ec30ec926dbf2796fd28aca83d09cc25
Paul Jewell <paul@teulu.org>
2021-02-23 11:29:58 +0000

    Added missing code

--- dwm.c
+++ dwm.c
@@ -36,6 +36,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
 #include <X11/Xutil.h>
+#include <X11/Xresource.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
 #endif /* XINERAMA */
@@ -205,6 +206,7 @@ static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *);
 static void propertynotify(XEvent *e);
+static void pushstack(comst Arg *arg);
 static void quit(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
 static void resize(Client *c, int x, int y, int w, int h, int interact);
@@ -1333,6 +1335,29 @@ propertynotify(XEvent *e)
 		if (ev->atom == netatom[NetWMWindowType])
 			updatewindowtype(c);
 	}
+}
+
+void
+pushstack(const Arg *arg) {
+  int i = stackpos(arg);
+  Client *sel = selmon->sel, *c, *p;
+  
+  if(i < 0 || !sel)
+    return;
+  else if(i == 0) {
+    detach(sel);
+    attach(sel);
+  }
+  else {
+    for(p = NULL, c = selmon->clients; c; p = c, c = c->next)
+      if(!(i -= (ISVISIBLE(c) && c != sel)))
+        break;
+    c = c ? c : p;
+    detach(sel);
+    sel->next = c->next;
+    c->next = sel;
+  }
+  arrange(selmon);
 }
 
 void
